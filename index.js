1ec06ad4ad12836da6a6c086d8b1ccb3210810f5
Noumanalone <163382172+Noumanalone@users.noreply.github.com>
2024-04-20 17:43:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 //*
  //  * @project_name : XLICON-V3-MD
  //  * @author : salmanytofficial
- //  * @youtube : https://www.youtube.com/@s4salmanyt
+ //  * @youtube : https://www.youtube.com/nyt
 //   * @description : XLICON-V3 ,A Multi-functional whatsapp user bot.
 //*
 //* 
@@ -28,7 +28,7 @@
 //Telegram: t.me/ahmmitech
 //GitHub: @salmanytofficial
 //WhatsApp: +923184070915
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@Deon
 //   * Created By Github: DGXeon.
 //   * Credit To Xeon
 //   * © 2024 XLICON-V3-MD.
